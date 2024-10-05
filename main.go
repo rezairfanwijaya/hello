6f88bf1a25effe09980758f91a8554931e0475b5
Reza Irfan Wijaya <rezaaagit@gmail.com>
2024-10-06 03:01:09 +0700

    feat: add simple http

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := map[string]string{
+			"message": "Hello, World!",
+		}
+
+		json.NewEncoder(w).Encode(response)
+	})
+
+	log.Println("starting the server on :8181")
+	if err := http.ListenAndServe(":8181", nil); err != nil {
+		log.Fatalf("failed run server on port 8181, err: %v", err)
+	}
 }
